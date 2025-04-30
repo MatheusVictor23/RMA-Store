4fd7a07cef267b609edf23552802765a76432b9c
Matheus Victor <matheuszaya@gmail.com>
2025-04-30 15:30:44 -0400

    feat: Adicionar funcionalidade login

---
+++ Cadastro/js/cadastro.js
@@ -0,0 +1,26 @@
+const button = document.querySelector("#cadastro_card_form_submit");
+
+
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    let nome = document.querySelector("#name");
+    let email = document.querySelector("#email");
+    let password = document.querySelector("#password");
+
+    if(nome.value.length > 3 && email.value.length > 3 && password.value.length > 3){
+        alert("Usuário cadastrado!")
+        nome.value = '';
+        email.value = '';
+        password.value = '';
+
+        setTimeout(() => {
+            window.location.href = "../../Login/index.html";
+        }, 1500);
+    }else{
+        alert("preencha os campos corretamente!");
+    }
+
+    
+    
+})
